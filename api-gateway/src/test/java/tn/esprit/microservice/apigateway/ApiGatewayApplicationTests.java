d39c960cda3aeb2dcd090e756413eaf7f25da365
Wassim Triki <wsmtriki@gmail.com>
2025-04-03 21:20:57 +0100

    setup repo and initiate api-gateway spring project

---
+++ api-gateway/src/test/java/tn/esprit/microservice/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.microservice.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
