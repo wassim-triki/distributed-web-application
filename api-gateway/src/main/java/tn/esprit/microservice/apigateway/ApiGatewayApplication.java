17e78af8251a0aa5d5c56c777309d6f0689415e2
chahe-dridi <chaher.dridi.100@gmail.com>
2025-04-04 08:40:29 +0100

    fixed api-gate and used it

--- api-gateway/src/main/java/tn/esprit/microservice/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/esprit/microservice/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,31 @@ package tn.esprit.microservice.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @Bean
+    public RouteLocator getwayRoutes (RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route("reclamation-service",r->r.path("/reclamations/**").uri("lb://reclamation-service"))
+                .build();
+    }
+
 
 }
