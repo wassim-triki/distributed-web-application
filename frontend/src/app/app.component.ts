e77c76c12ab6da897fd3b6803d0d73064427e029
Khouloud Belhadj <khouloud.belhadj@esprit.tn>
2025-04-24 21:20:45 +0100

    Add Footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,11 @@ import { KeycloakService } from '../app/services/keycloak.service';
 import { AsyncPipe } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./components/header/header.component"; 
-
+import { FooterComponent } from "./components/footer/footer.component"; // Import FooterComponent
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AsyncPipe, HeaderComponent], // Add AsyncPipe
+  imports: [CommonModule, RouterOutlet, AsyncPipe, HeaderComponent,FooterComponent], // Add AsyncPipe
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
