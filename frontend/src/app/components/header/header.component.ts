c5b494e48067cc089dd7c318e1244ba3c97833f8
chahe-dridi <chaher.dridi.100@gmail.com>
2025-04-24 16:24:48 +0100

    fixed desgin overall

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // Needed for routerLink to work in the template
-import { NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, NgIf],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
